48e8e2c370dc72df0a7fedcbd8fadde88713c487
Fabio <alessf13@gmail.com>
2021-09-12 16:30:03 +0200

    feat: getUser added with v-on

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ const app = Vue.createApp({
       picture: "https://randomuser.me/api/portraits/men/10.jpg",
     };
   },
+  methods: {
+    getUser() {
+      console.log(this.firstName);
+    },
+  },
 });
 
 app.mount("#app");
